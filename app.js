39e6fd50361ec7c1de1eaec690656f3632105606
Hellol77 <dhe7700@naver.com>
2021-08-23 03:56:28 +0900

    4.4 Getting Username

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const loginForm = document.querySelector("#login-form")
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginSubmit() {
+const link = document.querySelector("a");
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  if (username === "") {
-    alert("please write your name");
-  } else if (username.length>15) {
-    alert("your name is too long");
-  }
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
-
-```
+function handleLinkClick(event) {
+  console.log;
+  alert("click");
+}
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
