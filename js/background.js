364fc72b9d28c500e8a9fbfb28dba9c7ffea1932
Hellol77 <dhe7700@naver.com>
2021-08-27 23:51:53 +0900

    add align and font

--- js/background.js
+++ js/background.js
@@ -1,5 +1,6 @@
-const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+const images = ["img/0.jpg", "img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-document.body.appendChild(bgImage);
+
+document.body.style.backgroundImage = "url('"+chosenImage+"')"
+
